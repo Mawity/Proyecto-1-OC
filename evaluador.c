0795aa91068b818816d3168513e954ecfff2ba4e
Ezequiel Zucoli <42452277+zucolieze@users.noreply.github.com>
2020-10-23 10:51:08 -0300

    Add files via upload

--- evaluador.c
+++ evaluador.c
@@ -1,8 +1,19 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include "lista.h"
+#include "lista.c"
+#include "mapeo.h"
+#include "mapeo.c"
 
-int main()
-{
-    printf("Hello world!\n");
-    return 0;
+int main(){
+
+  char nombre_archivo[30];
+  printf("%s\n","Ingrese el nombre del archivo");
+  scanf("%s",nombre_archivo);
+
+
+
+
+
+  return 0;
 }
